e406ac39b9095192108a7d31eb30dbd78b64befd
Jan Adámek <adamejan2@seznam.cz>
2026-01-19 10:50:32 +0100

    to do add ingame data, this will be fun

--- src/IngameSys/Control.java
+++ src/IngameSys/Control.java
@@ -1,5 +1,11 @@
 package IngameSys;
+import com.google.gson.Gson;
+import com.sun.tools.javac.Main;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Control {
@@ -27,9 +33,34 @@ public class Control {
     public void save(){
         //TODO dat data z mapy do souboru
     }
-    public void load(){
+    public static Control load(String resourcePath){
         //TODO nacist data ze souboru do hashmapy data
+        Gson gson = new Gson();
+
+        //Načtení souboru gamedata.json, musí být ve složce res/resources, ta musí být označena jako resource složka projektu
+        try (InputStream is = Main.class.getResourceAsStream(resourcePath)) {
+
+            //Zde ověřujeme, zdali soubor existuje
+            if (is == null) {
+                throw new IllegalStateException("Nenalezen resource: " + resourcePath +
+                        " (zkontrolujte, že soubor je v src/main/resources).");
+            }
+
+            //Přečte celý JSON a vytvoří instanci GameData, naplní vlastnosti podle názvů klíčů v JSONU, vrátí se hotová třída GameData
+            return gson.fromJson(
+                    new InputStreamReader(is, StandardCharsets.UTF_8),
+                    Control.class
+            );
+
+        } catch (Exception e) {
+            throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
+        }
+
+
+
+
     }
+
 
 
 
